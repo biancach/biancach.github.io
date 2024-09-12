73081fd26ee2e5a41368bc7345ecc44e418d7ed2
Bianca <bianca@dhcp-10-29-128-229.dyn.MIT.EDU>
2024-09-11 22:13:13 -0400

    update

--- fundraiser.js
+++ fundraiser.js
@@ -138,7 +138,7 @@ function drawGraph(miles, money) {
 }
 
 // Initial draw
-const donation = 360;
+const donation = 420;
 const matched = 1415.2;
 const total = donation + 2*matched
 const miles = [21, 20, 31, 30, 30, 36, 37, 40, 13];
